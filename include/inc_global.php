5dbddfbc4fa0ba8135ae605570c654853703cc42
Ponkrit Eiamprasertkul <ponkrit13@gmail.com>
2014-09-02 22:30:58 +0700

    Create inc_global.php

---
+++ include/inc_global.php
@@ -0,0 +1,7 @@
+<?
+define(SITE_NAME, 'http://txfunds.uat.ipayoptions.com');
+define(LOGIN_ACT, 'login');
+define(USERCHECK_ACT, 'usercheck');
+define(LOGIN_EMAIL, 'apitest@ipayoptions.com');
+define(LOGIN_PWD, '1q2w3e4r5t6y');
+?>
